140a3e009d14bce36779a34fc47c62c50c94b501
Pranjal Choubay <your_github_email@example.com>
2025-08-17 14:15:26 +0530

    Chat first

--- src/theme.js
+++ src/theme.js
@@ -3,8 +3,8 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
   palette: {
     mode: "light",
-    primary: { main: "#1976d2" },
-    background: { default: "#fafafa" }
+    primary: { main: "#6c1dc7ff" },
+    background: { default: "#000000ff" }
   },
   shape: { borderRadius: 14 }
 });
